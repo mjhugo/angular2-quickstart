953d888991c058dbf75318393367b54c818b627b
Mike Hugo <mike@piragua.com>
2016-05-13 12:35:26 -0500

    attempt at using bs4 theme

--- app/boot.ts
+++ app/boot.ts
@@ -1,6 +1,9 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component'
 import {enableProdMode} from '@angular/core';
+import {Ng2BootstrapConfig, Ng2BootstrapTheme} from 'ng2-bootstrap/ng2-bootstrap';
+
+Ng2BootstrapConfig.theme = Ng2BootstrapTheme.BS4;
 
 enableProdMode();
 
